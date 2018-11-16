5cb88b4c70e93ee98849cd0d4397f8534b30f4c8
Oscar Isomäki <a1400239@myy.haaga-helia.fi>
2018-11-16 19:46:52 +0200

    added minionsetup.sh

---
+++ scripts/minionsetup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#salt-minion first time conf
+
+HOSTNAME="$(hostname -I)"
+#1 if salt-master is installed, 0 if not
+MASTEREXISTS="$(dpkg-query -l | grep salt-master | wc -l)"
+echo "input desired id for minion"
+read SLAVE
+
+sudo apt-get update
+sudo apt-get -y install salt-minion
+
+#checks if salt-master is installed, so we don't have to input hostname
+#manually when installing minion and master on the same computer
+if [ ${MASTEREXISTS} == 1 ]
+then
+	echo -e 'master: '$HOSTNAME'\nid: '$SLAVE|sudo tee /etc/salt/minion
+else
+	echo "enter master hostname"
+	read MANUALHOSTNAME
+	echo -e 'master: '$MANUALHOSTNAME'\nid: '$SLAVE|sudo tee /etc/salt/minion
+fi
+sudo systemctl restart salt-minion.service
+echo 'Minion setup complete!'
