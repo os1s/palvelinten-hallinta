303c94b60da699f75b2c1be54714653ed91c13e1
os1s <a1400239@myy.haaga-helia.fi>
2018-11-26 20:10:36 +0200

    added firefox files

---
+++ tulikettu/user.js
@@ -0,0 +1,4 @@
+user_pref("browser.urlbar.placeholderName", "Tällä näkee helposti onko muutosta tapahtunut :)");
+user_pref("browser.shell.checkDefaultBrowser", false);
+
+//C:\Users\scrsm\AppData\Roaming\Mozilla\Firefox\Profiles\osku\user.js
